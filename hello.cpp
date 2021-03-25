ab9580b546e5d3652ff937cb36cb972db26b2832
Aditya Vishal Singh <81331427+aditya2021-vishu@users.noreply.github.com>
2021-03-25 18:53:47 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
 
-    cout<<"HEELO WORLD!! THIS IS MY FIRST REPOSITORY"<<endl;
+    cout<<"HEELO WORLD!! THIS IS MY 1st REPOSITORY"<<endl;
     cout<<"SIDDHARTH YADAV!!";
     return 0;
 }
